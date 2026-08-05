a1580c43e053f843363bd37c4148399764a4fd12
Shahab <itzshahab08@gmail.com>
2026-08-05 10:16:00 +0500

    Fix feature group version references and pm10 type mismatch

--- src/check_job.py
+++ src/check_job.py
@@ -7,7 +7,7 @@ HOPSWORKS_KEY = os.getenv("HOPSWORKS_API_KEY")
 
 project = hopsworks.login(api_key_value=HOPSWORKS_KEY)
 fs = project.get_feature_store()
-fg = fs.get_feature_group(name="aqi_features", version=3)
+fg = fs.get_feature_group(name="aqi_features", version=2)
 
 print("Current state:", fg.materialization_job.get_state())
 print("Final state:", fg.materialization_job.get_final_state())
--- src/restart_job.py
+++ src/restart_job.py
@@ -7,7 +7,7 @@ HOPSWORKS_KEY = os.getenv("HOPSWORKS_API_KEY")
 
 project = hopsworks.login(api_key_value=HOPSWORKS_KEY)
 fs = project.get_feature_store()
-fg = fs.get_feature_group(name="aqi_features", version=3)
+fg = fs.get_feature_group(name="aqi_features", version=2)
 
 print("Current state before restart:", fg.materialization_job.get_state())
 
